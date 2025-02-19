00eb4ce88895ef3b15cfce428812e4272c67db5e
Abrar Jaigirdar <jaigirdar111@gmail.com>
2025-02-19 17:59:07 -0500

    Implemented Issue 17: [Task] - Write Unit Tests for PerishableProduct Model on task/Unit-Test-for-PerishableProduct

--- src/test/java/org/myproject/model/PerishableProductTest.java
+++ src/test/java/org/myproject/model/PerishableProductTest.java
@@ -1,4 +1,22 @@
 package org.myproject.model;
 
-public class PerishableProductTest {
+import org.junit.jupiter.api.Test;
+import org.myproject.model.PerishableProduct;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PerishableProductTest {
+
+    @Test
+    void testExpiryDate() {
+        // Create a perishable product with an expiry date
+        PerishableProduct perishable = new PerishableProduct(102, "Milk", 20, new BigDecimal("2.99"), LocalDate.of(2025, 12, 31));
+
+        // Verify that the expiry date is set correctly
+        assertEquals(LocalDate.of(2025, 12, 31), perishable.getExpiryDate());
+    }
+
 }
